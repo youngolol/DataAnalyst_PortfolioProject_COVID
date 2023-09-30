b7a9c193923e886ec30f4a81c685aeed39ea4c45
Young <103384614+youngolol@users.noreply.github.com>
2023-09-30 16:25:21 -0700

    Add files via upload

---
+++ covid portfolio.sql
@@ -0,0 +1,7 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[ProgressiveVaccinations]
+  FROM [PortfolioProject].[dbo].[PercentVaccinated]
